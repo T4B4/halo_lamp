c73b31f9f44cc418f227f2f4c8e33893ece43611
T4B4 <claas.bruess@gmail.com>
2025-03-26 15:08:06 +0100

    invert false

--- worker_led_2.py
+++ worker_led_2.py
@@ -4,7 +4,7 @@ from rpi_ws281x import Color, PixelStrip
 
 LED_FREQ_HZ = 800000  # LED signal frequency in hertz (usually 800khz)
 LED_DMA = 10          # DMA channel to use for generating signal
-LED_INVERT = True    # True to invert the output signal (useful when using
+LED_INVERT = False    # True to invert the output signal (useful when using
                       #   NPN transistor level shift)
 LED_BRIGHTNESS = 255  # Set to 0 for darkest and 255 for brightest
 LED_CHANNEL = 0       # set to '1' for GPIOs 13, 19, 41, 45 or 53
